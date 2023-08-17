c368d567a76869450e4b43a205f902e28235e935
Weedomeker <weedomeker@gmail.com>
2023-08-17 13:40:01 +0200

    Text center Add dependance perf

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ const path = require ('path');
 
 const filePath = path.join(__dirname,'./public/deco/')
 async function modifyPdf (numCmd, ville, format, visuel, qte) {
-  const readPdf =  await fs.readFile(filePath + '2_100X200CM/100x205/DIBOND 100X205-5Galets 100x200_S_.pdf')
+  const readPdf =  await fs.readFile(filePath + '5_100X255CM/125x260/DIBOND 125X260-5Galets 100x255cm-S.pdf')
   const pdfDoc = await PDFDocument.load(readPdf)
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
 
   const pages = pdfDoc.getPages()
   const firstPage = pages[0]
   const { width, height } = firstPage.getSize()
-  firstPage.drawText(`${numCmd} - LM ${ville} - ${format}_${visuel}_${qte} EX(S)`, {
+  console.log('Format pdf: ', width + 'x' + height)
+
+  const text = `${numCmd} - LM ${ville} - ${format}_${visuel}_${qte} EX(S)`
+  const textSize = 30;
+  const textWidth = helveticaFont.widthOfTextAtSize(text, textSize);
+  //const textHeight = helveticaFont.heightAtSize(textSize);
+  firstPage.drawText(text, {
     x: 35,
-    y: height / 2,
-    size: 30,
+    y: height / 2 - textWidth / 2,
+    size: textSize,
     font: helveticaFont,
     color: rgb(0,0,0),
     rotate: degrees(90)
