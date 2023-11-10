eb58fd1d95b70f42dbda012886eaab6faac0d994
Weedomeker <weedomeker@gmail.com>
2023-11-10 12:42:41 +0100

    reverse log temp

--- client/src/components/Log.jsx
+++ client/src/components/Log.jsx
@@ -2,20 +2,24 @@ import PropTypes from 'prop-types';
 import { Message, List } from 'semantic-ui-react';
 
 function Log({ show, data }) {
-  data.forEach((item, i) => {
-    item.id = i + 1;
-  });
+  data &&
+    data.forEach((item, i) => {
+      item.id = i + 1;
+    });
+
   const options =
     data &&
-    data.map((v) => {
-      return (
-        <Message.List key={v.id}>
-          <div className="time"> {v.time}</div>
-          <List.Icon name="check" color="green" />
-          {v.value}
-        </Message.List>
-      );
-    });
+    data
+      .map((v) => {
+        return (
+          <Message.List key={v.id}>
+            <div className="time"> {v.time}</div>
+            <List.Icon name="check" color="green" />
+            {v.value}
+          </Message.List>
+        );
+      })
+      .reverse();
 
   if (show) {
     return <div className="log">{options}</div>;
