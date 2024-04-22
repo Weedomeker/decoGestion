856e0b22f0470a6cf704d621d3f2244e582d6413
Weedomeker <weedomeker@gmail.com>
2024-04-22 14:46:23 +0200

    Modification condition interval wastecut

--- src/wastcut.js
+++ src/wastcut.js
@@ -1,21 +1,32 @@
 function Wastcut(distance = Number, espacement = Number, name = String) {
   if (name == undefined) name = '';
+  let interval = distance / espacement;
 
-  const interval = distance / espacement;
+  if (interval > 3) interval = Math.floor(interval);
+
   if (distance < espacement) {
     console.log('Pas besoin de Wastcut');
   } else {
-    const coupe = parseFloat((distance / Math.round(interval)).toFixed(2));
+    //Nbr de decoupes
     console.log('Nbrs morceaux: ', interval);
+    const coupe = parseFloat((distance / Math.round(interval)).toFixed(2));
+
+    //Iteration decoupe par interval
     for (let i = 1; i < interval; i++) {
-      //console.log(`${name} ${i}: `, coupe * i);
-      console.log(
-        `${name}${i}: new makerjs.paths.Line([${coupe * i}, widthPlate / 2], [${coupe * i}, decWidth / 2 + 0.6]),`,
-      );
+      if (interval < 2) {
+        //Coupe en 2
+        console.log(
+          `${name}${i}: new makerjs.paths.Line([${coupe * i}, widthPlate / 2], [${coupe * i}, decWidth / 2 + 0.6]),`,
+        );
+      } else {
+        // console.log(`${name} ${i}: `, coupe * i);
+        console.log(
+          `${name}${i}: new makerjs.paths.Line([${coupe * i}, widthPlate / 2], [${coupe * i}, decWidth / 2 + 0.6]),`,
+        );
+      }
     }
   }
 }
 
-Wastcut(200, 80, 'v');
-
-// v3: new makerjs.paths.Line([decHeight / 3, widthPlate / 2], [decHeight / 3, decWidth / 2 + 0.6]),
+Wastcut(260, 80, 'v');
+Wastcut(100, 80, 'h');
