92a9f4b8bfff35d0ab65a605974be64bb2ef8c3e
Weedomeker <weedomeker@gmail.com>
2024-04-23 15:18:51 +0200

    update

--- src/wastcut.js
+++ src/wastcut.js
@@ -1,4 +1,6 @@
 function Wastcut(distance = Number, espacement = Number, name = String) {
+  let wastcut = [];
+  let layers = [];
   if (name == undefined) name = '';
   let interval = distance / espacement;
 
@@ -8,25 +10,39 @@ function Wastcut(distance = Number, espacement = Number, name = String) {
     console.log('Pas besoin de Wastcut');
   } else {
     //Nbr de decoupes
-    console.log('Nbrs morceaux: ', interval);
+    //console.log('Nbrs morceaux: ', interval);
     const coupe = parseFloat((distance / Math.round(interval)).toFixed(2));
 
     //Iteration decoupe par interval
+
     for (let i = 1; i < interval; i++) {
+      let negative;
+      i < 2 ? (negative = '-') : (negative = '');
+
       if (interval < 2) {
-        //Coupe en 2
-        console.log(
-          `${name}${i}: new makerjs.paths.Line([${coupe * i}, widthPlate / 2], [${coupe * i}, decWidth / 2 + 0.6]),`,
-        );
+        //Coupe en 2 si moins de deux morceaux
+        console.log('decoupe en deux');
       } else {
-        // console.log(`${name} ${i}: `, coupe * i);
-        console.log(
-          `${name}${i}: new makerjs.paths.Line([${coupe * i}, widthPlate / 2], [${coupe * i}, decWidth / 2 + 0.6]),`,
+        //Waste Top
+        wastcut.push(
+          `${name}Top${i}: new makerjs.paths.Line([${negative}${coupe * i}, widthPlate / 2], [${negative}${
+            coupe * i
+          }, decWidth / 2 + 0.6]),`,
         );
+        layers.push(`model.models.wasteCut.paths.${name}Top${i}.layer = 'maroon';`);
+
+        //Waste Bottom
+        wastcut.push(
+          `${name}Bottom${i}: new makerjs.paths.Line([${negative}${coupe * i}, -widthPlate / 2], [${negative}${
+            coupe * i
+          }, -decWidth / 2 - 0.6]),`,
+        );
+        layers.push(`model.models.wasteCut.paths.${name}Bottom${i}.layer = 'maroon';`);
       }
     }
   }
+  console.log(wastcut.sort());
+  console.log(layers.sort());
 }
 
 Wastcut(260, 80, 'v');
-Wastcut(100, 80, 'h');
