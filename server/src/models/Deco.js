12ba3f8f7af273188ddeaf3ca2f004e8af9b6000
weedomeker <weedomeker@gmail.com>
2025-10-08 19:25:23 +0200

    gitignore

--- server/src/models/Deco.js
+++ server/src/models/Deco.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const RefDeco = require('./RefDeco');
 
+// Schéma des commandes
 const decoSchema = new mongoose.Schema({
   date: { type: Date },
   numCmd: { type: Number },
@@ -8,6 +10,7 @@ const decoSchema = new mongoose.Schema({
   deco: { type: String },
   ref: { type: Number },
   format: { type: String },
+  finition: { type: String, default: '' },
   ex: { type: Number },
   temps: { type: Number },
   perte: { type: Number },
@@ -16,6 +19,53 @@ const decoSchema = new mongoose.Schema({
   ip: { type: String },
 });
 
-const Deco = mongoose.model('lm_commandes', decoSchema);
+// Hook avant save
+decoSchema.pre('save', async function (next) {
+  try {
+    if (this.isModified('ref') && this.ref) {
+      const refData = await RefDeco.findOne({ 'REF LM': this.ref });
+
+      if (refData) {
+        this.finition = refData['FINITION VERNIS'] || '';
+        this.format = refData['FORMAT'] || this.format;
+        this.deco = refData['NOM DU MODELE'] || this.deco;
+      } else {
+        // si pas trouvé → vider finition et déco
+        this.finition = '';
+      }
+    }
+    next();
+  } catch (err) {
+    console.error('Erreur pre-save:', err);
+    next(err);
+  }
+});
+
+// Hook avant update (findOneAndUpdate)
+decoSchema.pre('findOneAndUpdate', async function (next) {
+  try {
+    const update = this.getUpdate();
+
+    if (update.ref) {
+      const refData = await RefDeco.findOne({ 'REF LM': update.ref });
+
+      if (refData) {
+        update.finition = refData['FINITION VERNIS'] || '';
+        update.format = refData['FORMAT'] || update.format;
+        update.deco = refData['NOM DU MODELE'] || update.deco;
+      } else {
+        update.finition = '';
+      }
+
+      this.setUpdate(update);
+    }
+    next();
+  } catch (err) {
+    console.error('Erreur pre-findOneAndUpdate:', err);
+    next(err);
+  }
+});
+
+const Deco = mongoose.model('Deco', decoSchema, 'lm_commandes');
 
 module.exports = Deco;
---
+++ server/src/models/RefDeco.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const refDecoSchema = new mongoose.Schema(
+  {
+    'REF LM': { type: Number, required: true },
+    'NOM DU MODELE': { type: String },
+    'FINITION VERNIS': { type: String },
+    FORMAT: { type: String },
+  },
+  { collection: 'lm_ref_deco' },
+); // préciser le nom exact de la collection Mongo
+
+const RefDeco = mongoose.model('RefDeco', refDecoSchema);
+
+module.exports = RefDeco;
